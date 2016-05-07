9170825fc010dc336605b682553ef5f6b8187345
Windu Purnomo <wpwindupurnomo@gmail.com>
2016-05-07 09:00:56 +0700

    Bootstrapping a spring boot application project

---
+++ src/main/java/com/zcodegroup/App.java
@@ -0,0 +1,17 @@
+package com.zcodegroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot main application
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
