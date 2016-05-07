5d50731dc5534788886934324be2694707c36091
Windu Purnomo <wpwindupurnomo@gmail.com>
2016-05-07 14:05:07 +0700

    web service CRUD in memmory

--- src/main/java/com/zcodegroup/ws/api/UserController.java
+++ src/main/java/com/zcodegroup/ws/api/UserController.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +28,30 @@ public class UserController {
 			users = new HashMap<BigInteger, User>();
 			nextId = BigInteger.ONE;
 		}
+		
+		// if update...
+		if (user.getId() != null){
+			User oldUser = users.get(user.getId());
+			if (oldUser == null){
+				return null;
+			}
+			users.remove(user.getId());
+			users.put(user.getId(), user);
+			return user;
+		}
+		
+		// if create ...
 		user.setId(nextId);
 		nextId = nextId.add(BigInteger.ONE);
 		users.put(user.getId(), user);
 		return user;
 	}
 
+	private static boolean delete(BigInteger id){
+		User deletedUser = users.remove(id);
+		return deletedUser != null;
+	}
+	
 	static {
 		User u1 = new User();
 		u1.setName("Windu Purnomo");
@@ -47,4 +67,48 @@ public class UserController {
 		Collection<User> u = users.values();
 		return new ResponseEntity<Collection<User>>(u, HttpStatus.OK);
 	}
+
+	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getUser(@PathVariable("id") BigInteger id){
+		User user = users.get(id);
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/user", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User createdUser = save(user);
+		return new ResponseEntity<User>(createdUser, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/user", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> updateUser(@RequestBody User user){
+		User updatedUser = save(user);
+		if (updatedUser == null){
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> deleteUser(@PathVariable("id") BigInteger id){
+		boolean isDeleted = delete(id);
+		if (!isDeleted){
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}else{
+			return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
 }
